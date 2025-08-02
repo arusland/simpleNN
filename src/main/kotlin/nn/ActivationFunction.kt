03dafc176ed985f43d0ce2f7fb415c9b56f76fdb
Ruslan A. <ruslan.absalyamov@gmail.com>
2025-08-02 16:52:25 +0200

    Add activation function: tahn

--- src/main/kotlin/nn/ActivationFunction.kt
+++ src/main/kotlin/nn/ActivationFunction.kt
@@ -9,5 +9,13 @@ enum class ActivationFunction(
     SIGMOID(
         activation = { x -> 1.0 / (1.0 + exp(-x)) },
         derivative = { x -> x * (1.0 - x) }
-    );
+    ),
+    TANH(
+        activation = { x ->
+            val exp = exp(x)
+            val expMinus = exp(-x)
+            (exp - expMinus) / (exp + expMinus)
+        },
+        derivative = { x -> 1.0 - x * x }
+    ),
 }
